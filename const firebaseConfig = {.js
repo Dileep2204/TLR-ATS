e090bc09672769a9e67c0591e77628da51aa600e
Dileep2204 <dileepthulugu@gmail.com>
2026-05-16 01:11:33 +0530

    All files added for attendance tracker

---
+++ const firebaseConfig = {.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "మీ_API_KEY",
+  authDomain: "మీ_PROJECT_ID.firebaseapp.com",
+  projectId: "మీ_PROJECT_ID",
+  storageBucket: "మీ_PROJECT_ID.appspot.com",
+  messagingSenderId: "మీ_SENDER_ID",
+  appId: "మీ_APP_ID"
+};
+const appId = "tlr-attendance-app"; 
